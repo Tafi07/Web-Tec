4dd7ec27ca355fd2a500d8168dc2ea7f3d87b4a8
Tafi07 <91612688+Tafi07@users.noreply.github.com>
2022-06-14 21:41:36 +0600

    Add files via upload
    This a File Containing All The Task From Lab 3

---
+++ Lab Task 3/Profile Picture/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  
+    <h1 style="color: tomato;"> Profile Picture</h1>
+    <img src="Uploadpic.png" alt="Upload Picture"style="width:200px;height:200px";><br>
+
+
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
